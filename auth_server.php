7f0979695a30eb6ea1141d2867106d046aedd2a9
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-25 10:39:55 -0600

    Bugfix en servidor de autenticacion

--- auth_server.php
+++ auth_server.php
@@ -5,7 +5,7 @@ $method = strtoupper( $_SERVER['REQUEST_METHOD'] );
 $token = "5d0937455b6744.68357201";
 
 if( $method === 'POST' ){
-    if( !array_key_exists('HTTP_X_CLIENT_ID', $_SERVER || !array_key_exists('HTTP_X_SECRET', $_SERVER) ) ){
+    if( !array_key_exists('HTTP_X_CLIENT_ID', $_SERVER) || !array_key_exists('HTTP_X_SECRET', $_SERVER) ){
         http_response_code( 400 );
 
         die('Faltan parametros');
@@ -14,7 +14,7 @@ if( $method === 'POST' ){
     $clientId = $_SERVER['HTTP_X_CLIENT_ID'];
     $secret = $_SERVER['HTTP_X_SECRET'];
 
-    if( !$clientID !== '1' || $secret !== 'SecretoDeEstado' ){
+    if( $clientId !== '1' || $secret !== 'SecretoDeEstado' ){
         http_response_code( 403 );
 
         die('Sin autorización');
